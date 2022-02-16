3837c99f24dc23ffcbbff82c81c581842bd4e479
Ethan-Genser <ethanp1033@gmail.com>
2022-02-15 21:44:17 -0600

    Added starting framework for player entry screen

--- playerEntry.js
+++ playerEntry.js
@@ -1,1 +1,35 @@
+// Maximum number of players on each team.
+const MAX_PLAYERS = 10;
 
+class Player {
+    constructor(idNumber, codeName) {
+        this.idNumber = idNumber;
+        this.codeName = codeName;
+    }
+}
+
+function onSubmit() {
+    // Gets the idNumber and codeName for each red player and submits it to the database.
+    for (let i = 1; i < MAX_PLAYERS; i++) {
+        let idNumber = document.getElementById("redIdNumber" + i).value;
+        let codeName = document.getElementById("redCodeName" + i).value;
+        let player = new Player(idNumber, codeName);
+
+        submitPlayer(player);
+    }
+
+    // Gets the idNumber and codeName for each green player and submits it to the database.
+    for (let i = 1; i < MAX_PLAYERS; i++) {
+        let idNumber = document.getElementById("greenIdNumber" + i).value;
+        let codeName = document.getElementById("greenCodeName" + i).value;
+        let player = new Player(idNumber, codeName);
+
+        submitPlayer(player);
+    }
+}
+
+function submitPlayer(player) {
+
+    // To do: submit player info to database.
+
+}
