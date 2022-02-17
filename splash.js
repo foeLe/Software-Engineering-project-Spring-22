276a1df9383b676d208c8c397f6a2e91667554bc
CoolCatCB <callumbruton@gmail.com>
2022-02-17 13:47:24 -0600

    Rough draft of my current idea.

--- splash.js
+++ splash.js
@@ -2,4 +2,13 @@
 //Description: This JavaScript (JS) file entire goal is to handle the displaying of the logo on first load.
 //Date: 02-16-2022
 
+function waitTime() //This is the waiting function that will have sit on the current page for three seconds.
+{
+    console.log("Hello I have loaded!");
+    nextPage();
+}
 
+function nextPage() //This will be called once the timer is done and sends us to the player-add screen.
+{
+    console.log("Hello next page has been called but I have no page to load.")
+}
