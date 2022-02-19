3a7b7a2c10def892410e0389c25d08c1bf99b6c8
CoolCatCB <CallumBruton@gmail.com>
2022-02-19 11:25:04 -0800

    Removed print statements

--- splash.js
+++ splash.js
@@ -4,13 +4,11 @@
 
 function waitTime() //This is the waiting function that will have sit on the current page for three seconds.
 {
-    console.log("Hello I have loaded!");
 	setTimeout(nextPage, 3000);
 }
 
 function nextPage() //This will be called once the timer is done and sends us to the player-add screen.
 {
-    console.log("Hello next page has been called but I have no page to load.");
     //document.getElementById('logo').remove(); //I was thinking that instead of removing the logo we should just jump directly to the next page.
     document.location.href="playerEntry.html"; 
 }
