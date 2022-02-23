2d157ae26cffb932ca2b94fb1e275e9dc1d163c9
foeLe <98555641+foeLe@users.noreply.github.com>
2022-02-22 22:03:09 -0600

    Update splash.js

--- public/stylesheets/splash.js
+++ public/stylesheets/splash.js
@@ -4,12 +4,13 @@
 
 function waitTime() //This is the waiting function that will have sit on the current page for three seconds.
 {
-	setTimeout(nextPage, 3000);
+// 	setTimeout(nextPage, 3000);
+	setTimeout(null, 3000);
 }
 
-function nextPage() //This will be called once the timer is done and sends us to the player-add screen.
-{
-    document.getElementById('logo').remove();
-    document.location.href="views/pages/playerEntry.ejs"; 
-}
+// function nextPage() //This will be called once the timer is done and sends us to the player-add screen.
+// {
+//     document.getElementById('logo').remove();
+//     document.location.href="views/pages/playerEntry.ejs"; 
+// }
 
