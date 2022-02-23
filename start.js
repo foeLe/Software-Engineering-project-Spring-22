3c2fa6b26b6c799734c047222c068b053db901d8
sy-hong <81196274+sy-hong@users.noreply.github.com>
2022-02-23 15:25:01 -0600

    fixing screen change
    comments under 'start.js'

--- start.js
+++ start.js
@@ -5,10 +5,28 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/splash'))
-//   .get('/', (req, res) => res.render('pages/playerEntry'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+/**
+ * To direct from a page to another page, we need a line that builds a route first 
+ * before using regular html/ejs redirecting codes under '.ejs' files.
+ * EX:
+ *    1) A line that builds a route for 'playerEntry':
+ *      - '.get('/playerEntry', (req, res) => res.render('pages/playerEntry'))'
+ * 
+ *       ** this line of code would make the playerEntry web be named: 
+ *          "https://team-11-app.herokuapp.com/playerEntry"
+ *       ** similar, since we did not specify a specific name for the splash screen as  
+ *          the program starts, the initial app web would be (for 3 seconds):
+ *          "https://team-11-app.herokuapp.com"
+ *          
+ * 
+ *    2) Under 'splash.ejs,' add in the line that redirects to 'player.ejs.'
+ *       Since we are redirectly automatically after 3 seconds, we use: 
+ *      - '<meta http-equiv = "refresh" content = "3; url = https://team-11-app.herokuapp.com/playerEntry" />'     
+ */
+ express()
+ .use(express.static(path.join(__dirname, 'public')))
+ .set('views', path.join(__dirname, 'views'))
+ .set('view engine', 'ejs')
+ .get('/', (req, res) => res.render('pages/splash')) 
+ .get('/playerEntry', (req, res) => res.render('pages/playerEntry'))
+ .listen(PORT, () => console.log(`Listening on ${ PORT }`))
