01af01753c23a44ae4bd7a495633e7014a8c5a8f
Auspat <austinrodriguez904@gmail.com>
2022-02-27 16:50:20 -0600

    s

--- start.js
+++ start.js
@@ -56,7 +56,6 @@ const f = function(){    //test function to see if export works
 // }
 
  express()
- .use(express.static(path.join(__dirname, 'public')))
  .set('views', path.join(__dirname, 'views'))
  .set('view engine', 'ejs')
  .get('/', (req, res) => res.render('pages/splash')) 
