9f7abff9ecce58684a494b50b04d09e246a2e679
Auspat <austinrodriguez904@gmail.com>
2022-02-27 16:17:17 -0600

    science

--- start.js
+++ start.js
@@ -84,4 +84,63 @@ const f = function(){    //test function to see if export works
  
 module.exports = f;
 
- 
+ //const x = require("./test");         //testing simple import
+
+// Maximum number of players on each team.
+const MAX_PLAYERS = 15;
+
+class Player {
+    constructor(idNumber, codeName) {
+        this.idNumber = idNumber;
+        this.codeName = codeName;
+    }
+}
+
+function onSubmit() {
+    // Gets the idNumber and codeName for each red player and submits it to the database.
+    for (let i = 1; i <= MAX_PLAYERS; i++) {
+        let idNumber = document.getElementById("redIdNumber" + i).value;
+        let codeName = document.getElementById("redCodeName" + i).value;
+
+		// Check to make sure the player info is not blank before submitting player
+		if (idNumber != 0 && idNumber != "" && codeName != "") {
+			let player = new Player(idNumber, codeName);
+			
+			// Submit player to database
+			submitPlayer(player);
+		}
+    }
+
+    // Gets the idNumber and codeName for each green player and submits it to the database.
+    for (let i = 1; i <= MAX_PLAYERS; i++) {
+        let idNumber = document.getElementById("greenIdNumber" + i).value;
+        let codeName = document.getElementById("greenCodeName" + i).value;
+
+		// Check to make sure the player info is not blank before submitting player
+		if (idNumber != 0 && idNumber != "" && codeName != "") {
+			let player = new Player(idNumber, codeName);
+			
+			// Submit player to database
+			submitPlayer(player);
+		}
+    }
+}
+
+function submitPlayer(player) {
+
+    // To do: submit player info to database.
+    console.log("submitPlayer function here")
+    console.log(player.idNumber + " " + player.codeName)
+    //x();
+    //startjs.insertUser(player.idNumber, player.codeName);
+    //startjs.print();
+    // startjs.(player.idNumber, player.codeName).then((result) => {
+    //     if(result){
+    //         console.log('player added')
+    //     }
+    // })
+}
+
+function hi(){
+    console.log("howdy");
+}
