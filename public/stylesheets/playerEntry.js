41f9a79aae8541b992a21c2934c701bc12f3b81b
Auspat <austinrodriguez904@gmail.com>
2022-02-26 20:22:52 -0600

    fixes

--- public/stylesheets/playerEntry.js
+++ public/stylesheets/playerEntry.js
@@ -1,4 +1,4 @@
-const startjs = Module.require('../../start');      //imports needed function for entering data into database
+const startjs = require('../../start');      //imports needed function for entering data into database
 
 // Maximum number of players on each team.
 const MAX_PLAYERS = 15;
