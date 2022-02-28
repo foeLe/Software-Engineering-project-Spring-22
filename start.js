7668eba048d8679bcb960d2bb4a18183a18c4af6
sy-hong <81196274+sy-hong@users.noreply.github.com>
2022-02-27 21:43:18 -0600

    submit database

--- start.js
+++ start.js
@@ -59,8 +59,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue2 = req.body.redIdNumber2;
     var codeNamePlayer2 = req.body.redCodeName2;
     if(idValue2 != 0 && idValue2 !="" || codeNamePlayer2 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue2+",'"+codeNamePlayer2+"')"
-    pool.query(sql, function (err) {
+    var sql2 = "insert into player (id, codeName) values("+idValue2+",'"+codeNamePlayer2+"')"
+    pool.query(sql2, function (err) {
       if (!err){
         res.send('success');
       } else {
@@ -73,8 +73,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue3 = req.body.redIdNumber3;
     var codeNamePlayer3 = req.body.redCodeName3;
     if(idValue3 != 0 && idValue3 !="" || codeNamePlayer3 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue3+",'"+codeNamePlayer3+"')"
-    pool.query(sql, function (err) {
+    var sql3 = "insert into player (id, codeName) values("+idValue3+",'"+codeNamePlayer3+"')"
+    pool.query(sql3, function (err) {
       if (!err){
         res.send('success');
       } else {
@@ -86,8 +86,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue4 = req.body.redIdNumber4;
     var codeNamePlayer4 = req.body.redCodeName4;
     if(idValue4 != 0 && idValue4 !="" || codeNamePlayer4 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue4+",'"+codeNamePlayer4+"')"
-    pool.query(sql, function (err) {
+    var sql4 = "insert into player (id, codeName) values("+idValue4+",'"+codeNamePlayer4+"')"
+    pool.query(sql4, function (err) {
       if (!err){
         res.send('success');
       } else {
@@ -100,8 +100,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue16 = req.body.greenIdNumber1;
     var codeNamePlayer16 = req.body.greenCodeName1;
     if(idValue16 != 0 && idValue16 !="" || codeNamePlayer16 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue16+",'"+codeNamePlayer16+"')"
-    pool.query(sql, function (err) {
+    var sql16 = "insert into player (id, codeName) values("+idValue16+",'"+codeNamePlayer16+"')"
+    pool.query(sql16, function (err) {
       if (!err){
         res.send('success');
       } else {
@@ -113,8 +113,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue17 = req.body.greenIdNumber2;
     var codeNamePlayer17 = req.body.greenCodeName2;
     if(idValue17 != 0 && idValue17 !="" || codeNamePlayer17 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue17+",'"+codeNamePlayer17+"')"
-    pool.query(sql, function (err) {
+    var sql17 = "insert into player (id, codeName) values("+idValue17+",'"+codeNamePlayer17+"')"
+    pool.query(sql17, function (err) {
       if (!err){
         res.send('success');
       } else {
@@ -126,8 +126,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue18 = req.body.greenIdNumber3;
     var codeNamePlayer18 = req.body.greenCodeName3;
     if(idValue18 != 0 && idValue18 !="" || codeNamePlayer18 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue18+",'"+codeNamePlayer18+"')"
-    pool.query(sql, function (err) {
+    var sql18 = "insert into player (id, codeName) values("+idValue18+",'"+codeNamePlayer18+"')"
+    pool.query(sql18, function (err) {
       if (!err){
         res.send('success');
       } else {
@@ -139,8 +139,8 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
     var idValue19 = req.body.greenIdNumber4;
     var codeNamePlayer19 = req.body.greenCodeName4;
     if(idValue19 != 0 && idValue19 !="" || codeNamePlayer19 !=""){
-    var sql = "insert into player (id, codeName) values("+idValue19+",'"+codeNamePlayer19+"')"
-    pool.query(sql, function (err) {
+    var sql19 = "insert into player (id, codeName) values("+idValue19+",'"+codeNamePlayer19+"')"
+    pool.query(sql19, function (err) {
       if (!err){
         res.send('success');
       } else {
