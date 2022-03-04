538e4b2f6be5717ebc8e6a02554a79239084c274
foeLe <98555641+foeLe@users.noreply.github.com>
2022-03-04 04:06:15 -0600

    implementing timer(?)

--- public/stylesheets/playerAction.js
+++ public/stylesheets/playerAction.js
@@ -1,8 +1,7 @@
 //Time in milliseconds to start the timer with (5 minutes = 300,000 ms)
 const TIMER_MS = 65000;
 
-
-function timerSetup() {
+function timerStart() {
 	var currentTime = new Date().getTime();
 	var endingTime = new Date(currentTime + TIMER_MS).getTime();
 
@@ -11,22 +10,26 @@ function timerSetup() {
 	var timerMin = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
 	var timerSec = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-	document.getElementById("timer").innerHTML = timerMin + ":" + timerSec;
-
-	var timerInterval = setInterval(timerTick, 1000);
-}
-	
-
-function timerTick() {
-	if(timeLeft < 0) {
-		clearInterval(timerInterval);
-		document.getElementById("timer").innerHTML = "Time's up!";
-	}
-	
-	else {
-		var timerMin = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-		var timerSec = Math.floor((timeLeft % (1000 * 60)) / 1000);
-		
+	if(timerSec < 10)
+		document.getElementById("timer").innerHTML = timerMin + ":0" + timerSec;
+	else
 		document.getElementById("timer").innerHTML = timerMin + ":" + timerSec;
-	}
+
+	var timerInterval = setInterval(function() {
+		timeLeft -= 1000;
+		if(timeLeft < 0) {
+			clearInterval(timerInterval);
+			document.getElementById("timer").innerHTML = "Time's up!";
+		}
+		
+		else {
+			var timerMin = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+			var timerSec = Math.floor((timeLeft % (1000 * 60)) / 1000);
+			
+			if(timerSec < 10)
+				document.getElementById("timer").innerHTML = timerMin + ":0" + timerSec;
+			else
+				document.getElementById("timer").innerHTML = timerMin + ":" + timerSec;
+		}
+	}, 1000);
 }
