39430e12f6c3cea9efd198e01a417fea7c434a85
Ethan Genser <ethanp1033@gmail.com>
2022-03-12 11:04:25 -0600

    test player array

--- start.js
+++ start.js
@@ -75,7 +75,7 @@ class Player {
             // Add player to current red team
             if (i < MAX_PLAYERS) {
               redTeam.push(new Player(id, codeName));
-              res.send("see id: " + redTeam[redTeam.length].getID());
+              res.send("see id: " + redTeam[redTeam.length - 1].getID());
             }
             // Add player to current green team
             else {
