0e7ddd380313500cb67f7b4df76e74602061054f
Ethan-Genser <ethanp1033@gmail.com>
2022-03-12 19:15:14 -0600

    rewriting player submit to use jquery

--- start.js
+++ start.js
@@ -175,7 +175,7 @@ class Player {
     // Wait 2 seconds (2000ms) to give pool.query time to add all players, then redirect to playerAction screen.
     setTimeout(function() {
       console.log("test");
-      res.render('pages/splash');
+      res.render('pages/');
     }, 2000)
   })
   // Sends client the current players on each team
