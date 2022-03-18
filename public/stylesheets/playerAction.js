f0e6bdb5908139ef229590bf6d9ce3443a8c91fe
Ethan-Genser <ethanp1033@gmail.com>
2022-03-17 21:13:11 -0500

    move playerAction jquery script from the ejs file to the js file.

--- public/stylesheets/playerAction.js
+++ public/stylesheets/playerAction.js
@@ -1,6 +1,20 @@
 //Time in milliseconds to start with (60,000 ms/minute)
 const TIMER_MS = 360000;
 
+// Retrieves current players from the server and fills in their info on the playerAction screen
+$(document).ready(function() {
+	$.get('https://team-11-app.herokuapp.com/players', {}, function(data){
+		for (let i = 0; i < data.redTeam.length && i < 15; i++) {
+			document.getElementById("redPlayer" + (i + 1)).innerHTML = data.redTeam[i].codeName;
+			document.getElementById("redPlayer" + (i + 1) + "Score").innerHTML = "0";
+		}
+		for (let i = 0; i < data.greenTeam.length && i < 15; i++) {
+			document.getElementById("greenPlayer" + (i + 1)).innerHTML = data.greenTeam[i].codeName;
+			document.getElementById("greenPlayer" + (i + 1) + "Score").innerHTML = "0";
+		}
+	})
+})
+
 function timerStart() {
     //Set timer length to the specified value in MS
 	var timeLeft = TIMER_MS;
