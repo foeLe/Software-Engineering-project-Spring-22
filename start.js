bb5767f8da623945b1291fc64ce06a499da842e9
Ethan-Genser <ethanp1033@gmail.com>
2022-04-06 18:05:40 -0500

    finding server ip

--- start.js
+++ start.js
@@ -34,6 +34,25 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
 });
 const dgram = require('dgram');
 const SOCKET_PORT = 7501;
+//test code
+const { networkInterfaces } = require('os');
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+console.log("------------------------");
+console.log(results);
+console.log("------------------------");
 
 // Maximum number of players per team
 const MAX_PLAYERS = 15;
@@ -46,12 +65,6 @@ class Player {
   constructor(idNumber, codeName) {
     this.idNumber = idNumber;
     this.codeName = codeName;
-  }
-  getID() {
-    return this.idNumber;
-  }
-  getName() {
-    return this.codeName;
   }
 }
 
