1880153981893e2ed0e7267d40e8f80e729f343d
Ethan-Genser <ethanp1033@gmail.com>
2022-04-07 19:50:39 -0500

    testing dgram

--- start.js
+++ start.js
@@ -33,25 +33,6 @@ const pool = new Pool({ // connects to our database (re-run 'npm install' since 
   }
 });
 const dgram = require('dgram');
-const SOCKET_PORT = 443;
-
-const { networkInterfaces } = require('os');
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-console.log(results);
 
 // Maximum number of players per team
 const MAX_PLAYERS = 15;
