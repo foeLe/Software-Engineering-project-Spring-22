6c25ff0b78569e91451e3a01c131d7fe2304401c
Ethan-Genser <ethanp1033@gmail.com>
2022-04-09 00:25:35 -0500

    fixing a mistake i made

--- start.js
+++ start.js
@@ -138,7 +138,6 @@ client.send([data1,data2],PORT,'localhost',function(error){
     console.log("-----------------------------------")
   }
 });
-const dgram = require('dgram');
 
 // Maximum number of players per team
 const MAX_PLAYERS = 15;
@@ -148,42 +147,6 @@ var redTeam = Array();
 var greenTeam = Array();
 
 class Player {
-<<<<<<< HEAD
-  constructor(idNumber, codeName) {
-    this.idNumber = idNumber;
-    this.codeName = codeName;
-  }
-}
-
-// Game traffic server
-// const server = dgram.createSocket('udp4');
-// server.on('listening', () => {
-//   let addr = server.address();
-//   console.log(`Listening for UDP packets at ${addr.address}:${addr.port}`);
-// });
-
-// server.on('message', (msg, rinfo) => {
-//   console.log('Recieved UDP message');
-// });
-// server.bind(PORT);
-
-const client = dgram.createSocket('udp4');
-client.connect(7501, '70.178.4.252', function() {
-    console.log("connection to 70.178.4.252:7501");
-    client.send("Connection established!");
-});
-client.on('message', (msg, rinfo) => {
-    console.log("message received!!");
-})
-
-// Express server
- express()
- .use(express.static(path.join(__dirname, 'public')))
- .use(bodyParser.urlencoded({extended:true}))
- .use(express.json())
- .set('views', path.join(__dirname, 'views'))
- .set('view engine', 'ejs')
-=======
     constructor(idNumber, codeName) {
         this.idNumber = idNumber;
         this.codeName = codeName;
@@ -202,7 +165,6 @@ const server = express()
 .use(express.json())
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
->>>>>>> eb2f67e570ace312f1ef85ddc725aa646d08871d
 
 // Views
 .get('/', (req, res) => res.render('pages/splash')) 
@@ -211,19 +173,9 @@ const server = express()
 .get('/playerAction', (req, res) => res.render('pages/playerAction'))
 .get('/playerEntry/submit', (req, res) => res.render('pages/playerAction'))
 
-<<<<<<< HEAD
- // Views
- .get('/', (req, res) => res.render('pages/splash')) 
- .get('/playerEntry', (req, res) => res.render('pages/playerEntry'))
- .get('/startTimer', (req, res) => res.render('pages/startTimer'))
- .get('/playerAction', (req, res) => res.render('pages/playerAction'))
- .get('/playerEntry/submit', (req, res) => res.render('pages/playerAction'))
- .get('/db', async (req, res) => {
-=======
 
 // Renders the content of the database to the clients browser.
 .get('/db', async (req, res) => {
->>>>>>> eb2f67e570ace312f1ef85ddc725aa646d08871d
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM player');
@@ -234,156 +186,6 @@ const server = express()
         console.error(err);
         res.send("Error " + err);
     }
-<<<<<<< HEAD
-  })
- 
-  // Sends client the current players on each team
- .get('/players', async (req, res) => {
-  // Store unique player info into a new array for each team
-  let newRedTeam = [];
-  let uniqueRed = {};
-  for (let i in redTeam){
-    let redID = redTeam[i]['idNumber'];
-    uniqueRed[redID] = redTeam[i];
-  }
-  for (i in uniqueRed) {
-    newRedTeam.push(uniqueRed[i])
-  }
-  
-  let newGreenTeam = [];
-  let uniqueGreen = {};
-  for (let i in greenTeam){
-    let greenID = greenTeam[i]['idNumber'];
-    uniqueGreen[greenID] = greenTeam[i];
-  }
-  for (i in uniqueGreen) {
-    newGreenTeam.push(uniqueGreen[i])
-  }
-
-  // Pass arrays with unique player(s) in each team for playerAction display
-  res.send({"redTeam": newRedTeam, "greenTeam": newGreenTeam});
- })
- .get('/playerEntry/checkIDs', async (req, res) => {
-  let redTeam = req.query.redTeam;
-  let greenTeam = req.query.greenTeam;
-  var searchId;
-  var searchName;
-  var parseRow;
-  var dbResult;
-
-  // -----------------------------------------------------------------------------
-  // TO DO:   Query the DB for each id in redTeam and greenTeam.
-  //          If the id already exists in the DB, change the name in redTeam or greenTeam arrays.
-  //          ex:
-  //          if (redTeam[i].id == dbQueryID) then redTeam[i].name == dbQueryName
-  // -----------------------------------------------------------------------------
-  for (let i = 0; i < redTeam.length; i++) {
-    // Query database for the count of an id
-    searchId = "SELECT COUNT(*) as total FROM player WHERE id = "+ redTeam[i].id +" " ; 
-    pool.query(searchId, function(err, result){
-      if (err)  {
-        res.send("Error " + err.message);
-      } 
-      else {
-        if (!result.length)  {
-          // Parse the result (dictionary) to get the specific count value
-          parseRow = result["rows"]
-          dbResult = parseRow[0].total
-          if (dbResult != 0) { 
-            // Pull the code name from the database
-            searchName = "SELECT codename as name FROM player WHERE id = "+ redTeam[i].id +" " ; 
-            pool.query(searchName, function(err, result){
-              if (err)  {
-                res.send("Error " + err.message);
-              } 
-              else { // Update the code name to the existing one
-                parseRow = result["rows"]
-                dbResult = parseRow[0].name
-                redTeam[i].name = dbResult
-              }
-            })
-          }
-          else { // New id - make sure it's code name is not duplicated
-            searchName = "SELECT COUNT(*) as total FROM player WHERE codename = '"+ redTeam[i].name +"' " ; 
-            pool.query(searchName, function(err, result){
-              if (err)  {
-                res.send("Error " + err.message);
-              } 
-              else {
-                if (!result.length)  {
-                  parseRow = result["rows"]
-                  dbResult = parseRow[0].total
-                  // Let users know that the name is taken
-                  if (dbResult != 0) { 
-                    redTeam[i].name = "<Name taken!>"
-                  }
-                }
-              }
-            })
-          }
-        }
-      }
-    })
-  }
-
-  for (let i = 0; i < greenTeam.length; i++) {
-    // Query database for the count of an id
-    searchId = "SELECT COUNT(*) as total FROM player WHERE id = "+ greenTeam[i].id +" " ; 
-    pool.query(searchId, function(err, result){
-      if (err)  {
-        res.send("Error " + err.message);
-      } 
-      else {
-        if (!result.length)  {
-          // Parse the result (dictionary) to get the specific count value
-          parseRow = result["rows"]
-          dbResult = parseRow[0].total
-          if (dbResult != 0) { 
-            searchName = "SELECT codename as name FROM player WHERE id = "+ greenTeam[i].id +" " ; 
-            pool.query(searchName, function(err, result){
-              if (err)  {
-                res.send("Error " + err.message);
-              } 
-              else { // Update the code name to the existing one
-                parseRow = result["rows"]
-                dbResult = parseRow[0].name
-                greenTeam[i].name = dbResult
-              }
-            })
-          }
-        }
-        else { // New id - make sure it's code name is not duplicated
-          searchName = "SELECT COUNT(*) as total FROM player WHERE codename = '"+ greenTeam[i].name +"' " ; 
-          pool.query(searchName, function(err, result){
-            if (err)  {
-              res.send("Error " + err.message);
-            } 
-            else {
-              if (!result.length)  {
-                parseRow = result["rows"]
-                dbResult = parseRow[0].total
-                // Let users know that the name is taken
-                if (dbResult != 0) { 
-                  greenTeam[i].name = "<Name taken!>"
-                }
-              }
-            }
-          })
-        }
-      }
-    })
-  }
-  
-  // Waits for arrays to properly update before sending the updated info back since used an 'async' function
-  setTimeout(function() {
-    // Sends client the updated lists of player data bacak to CheckIDs() in playerEntry.js
-    res.send({"redTeam": redTeam, "greenTeam": greenTeam});
-  }, 2000)
- })
-
-  // Receive player data submitted by client.
- .post('/playerEntry/submit', async (req, res) => {
-=======
 })
 // Sends client the current players on each team
 .get('/players', async (req, res) => {
@@ -525,7 +327,6 @@ const server = express()
 })
 // Receive player data submitted by client.
 .post('/playerEntry/submit', async (req, res) => {
->>>>>>> eb2f67e570ace312f1ef85ddc725aa646d08871d
     redTeam = Array();
     greenTeam = Array();
     var searchId;
@@ -533,86 +334,6 @@ const server = express()
     var dbResult;
 
     for (let i = 0; i < MAX_PLAYERS; i++) {
-<<<<<<< HEAD
-      let redID;
-      let redName;
-      let greenID;
-      let greenName;
-
-      if (i < req.body.redTeam.length) {
-        redID = req.body.redTeam[i].id;
-        redName =req.body.redTeam[i].name;
-      }
-      else {
-        redID = 0;
-        redName = "";
-      }
-      if (i < req.body.greenTeam.length) {
-        greenID = req.body.greenTeam[i].id;
-        greenName = req.body.greenTeam[i].name;
-      }
-      else {
-        greenID = 0;
-        greenName = "";
-      }
-
-      // add red player
-      if (redID != 0 && redID != "" && redName != "") {
-        // Add player to current green team for playerAction display
-        redTeam.push(new Player(redID, redName));
-
-        // Only insert new player into the database 
-        searchId = "SELECT COUNT(*) as total FROM player WHERE id = "+ redID +" " ; 
-        pool.query(searchId, function(err, result){
-          if (err)  {
-            res.send("Error " + err.message);
-          } 
-          else {
-            if (!result.length)  {
-              parseRow = result["rows"]
-              dbResult = parseRow[0].total
-              if (dbResult == 0) { 
-                var sql = "insert into player (id, codeName) values(" + redID + ", '" + redName + "')";
-                pool.query(sql, function (err) {
-                  if (err) 
-                    res.send("Error!");
-                })
-              }
-            }
-          }
-        })
-      }
-
-      // Add green player
-      if (greenID != 0 && greenID != "" && greenName != "") {
-        // Add player to current green team for playerAction display
-        greenTeam.push(new Player(greenID, greenName));
-
-        // Only insert new player into the database 
-        searchId = "SELECT COUNT(*) as total FROM player WHERE id = "+ greenID +" " ; 
-        pool.query(searchId, function(err, result){
-          if (err)  {
-            res.send("Error " + err.message);
-          } 
-          else {
-            if (!result.length)  {
-              parseRow = result["rows"]
-              dbResult = parseRow[0].total
-              if (dbResult == 0) { 
-                var sql = "insert into player (id, codeName) values(" + greenID + ", '" + greenName + "')";
-                pool.query(sql, function (err) {
-                  if (err) 
-                    res.send("Error!");
-                })
-              }
-            }
-          }
-        })
-      }
-    }
-  })
- .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-=======
         let redID;
         let redName;
         let greenID;
@@ -694,4 +415,3 @@ const server = express()
 })
 
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
->>>>>>> eb2f67e570ace312f1ef85ddc725aa646d08871d
