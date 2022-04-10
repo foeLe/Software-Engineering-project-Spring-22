317d1c2aca0694ee5137155f495ede1719dbc8fa
Ethan-Genser <ethanp1033@gmail.com>
2022-04-09 22:02:42 -0500

    testing websockets

--- public/stylesheets/playerAction.js
+++ public/stylesheets/playerAction.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
 			document.getElementById("greenPlayer" + (i + 1)).innerHTML = data.greenTeam[i].codeName;
 			document.getElementById("greenPlayer" + (i + 1) + "Score").innerHTML = "0";
 		}
-	})
-})
+	});
+});
 
 function timerStart() {
     //Set timer length to the specified value in MS
@@ -65,3 +65,29 @@ function timerStart() {
 		}
 	}, 1000);
 }
+
+let socket = new WebSocket("wss://team-11-app.herokuapp.com/playerAction");
+
+socket.onopen = function(e) {
+  alert("[open] Connection established");
+  alert("Sending to server");
+  socket.send("My name is John");
+};
+
+socket.onmessage = function(event) {
+  alert(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    alert('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
